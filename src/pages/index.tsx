c9fd213b6c97fb0d07beadbdc4e77c0b094496b9
Ming <harry21105@gmail.com>
2025-09-05 17:30:31 +0800

    Fix homepage redirect to /intro

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,5 @@
-import type {ReactNode} from 'react';
 import {Redirect} from '@docusaurus/router';
 
-export default function Home(): ReactNode {
-  // Redirect to docs intro page
-  return <Redirect to="/docs/intro" />;
+export default function Home(): JSX.Element {
+  return <Redirect to="/intro" />;
 }
